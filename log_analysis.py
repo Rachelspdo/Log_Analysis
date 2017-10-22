6c063fa197ae57f335b0fecb5c1183d2688cb51a
Rachel Do <rachelspdo20@gmail.com>
2017-10-22 16:48:45 -0400

    first commit

---
+++ log_analysis.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+DBNAME = "news"
+
+# Most Three Popular Articles
+
+query_1 ="SELECT * FROM Summary LIMIT 3;"
+
+# Find the most popular article authors
+
+query_2 ="SELECT Popular_author.author, authors.name, Popular_author.Total_Views FROM authors, Popular_author WHERE authors.id = Popular_author.author LIMIT 1;"
+
+# Find the day which has more than 1% or 0.01 of requests lead to errors
+
+query_3="SELECT DATE_REQUEST, (1.00 * TOTAL_ERRORS / TOTAL_REQUESTS) as PECG FROM Access_Summary_By_Date WHERE (1.00 * TOTAL_ERRORS / TOTAL_REQUESTS) > 0.01;"
+
+
+def get_query_result(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query_1)
+    results = c.fetchall()
+    db.close()
+    print ("Popular Articles:")
+    for row in rows:
+        print ("", row[0])
+    return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
