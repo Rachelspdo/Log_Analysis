7025ad7b9c04323e727bdca0d9cd7bcfd999832f
Rachel Do <rachelspdo20@gmail.com>
2017-10-23 14:00:37 -0400

    Get The Top Three Popular Articles

--- log_analysis.py
+++ log_analysis.py
@@ -2,9 +2,6 @@ import psycopg2
 
 DBNAME = "news"
 
-# Most Three Popular Articles
-
-query_1 ="SELECT * FROM Summary LIMIT 3;"
 
 # Find the most popular article authors
 
@@ -18,17 +15,30 @@ query_3="""SELECT DATE_REQUEST, (1.00 * TOTAL_ERRORS / TOTAL_REQUESTS) as PECG
 FROM Access_Summary_By_Date
 WHERE (1.00 * TOTAL_ERRORS / TOTAL_REQUESTS) > 0.01;"""
 
+# get_query_result function is used to execute a SQL query
 
-def get_query_1_result(query):
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute(query_1)
-    results = c.fetchall()
-    db.close()
-    print ("Popular Articles:")
-    for row in rows:
-        print ("", row[0])
-    return results
+def get_query_result(query):
+    try:
+        db = psycopg2.connect(database=DBNAME)
+        c = db.cursor()
+        c.execute(query_1)
+        results = c.fetchall()
+        db.close()
+        return results
+
+# Get the top three articles by views
+
+def top_three_popular_articles():
+    query_1 ="SELECT * FROM Summary LIMIT 3;"
+    popular_articles= get_query_result(query_1)
+    print('Question 1: Most popular three articles of all times:")
+    for i in popular_articles:
+          print('"' + i[0] + '"' + ' -- ' + str(i[1]) + 'views')
+          print(' ')
+
+print(' ')
+    
+          
 
 
 
